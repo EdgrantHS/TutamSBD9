93570bf3a8ad6a92556aa791c22f39595e6721a2
EdgrantHS <edgrant.h@gmail.com>
2024-05-28 16:07:48 +0700

    docker

--- tutam9-BE/index.js
+++ tutam9-BE/index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 //Routes
-const userRoutes = require("./routes/userRoute");
+const userRoutes = require("./routes/UserRoute");
 
 const express = require("express");
 const cors = require("cors");
